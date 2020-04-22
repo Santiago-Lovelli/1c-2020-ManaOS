5858a9aa10a7c4cabd8e9bef8df604f4238f061f
Juan Anello <juan.manuel.anello@gmail.com>
2020-04-21 21:04:38 -0300

    Comienzo a trabajar en Gameboy

--- Gameboy/src/GameBoy.c
+++ Gameboy/src/GameBoy.c
@@ -1,12 +1,30 @@
 #include "GameBoy.h"
 
 int main(void) {
-	t_config* archivo_de_configuracion = config_create("/home/utnso/workspace/tp-2020-1c-ManaOS-/GameCard/GameCard.config");
-	char *puerto = config_get_string_value(archivo_de_configuracion, "PUERTO_GAMECARD");
-	char *ip = config_get_string_value(archivo_de_configuracion, "IP_GAMECARD");
-	t_log *logger = iniciar_log("GameBoy");
-	int conexion = conectarse_a_un_servidor(ip,puerto,logger);
+	t_config* archivo_de_configuracion = config_create("../Gameboy.config");
 
+	char *puertoBroker = config_get_string_value(archivo_de_configuracion, "PUERTO_BROKER");
+	char *ipBroker = config_get_string_value(archivo_de_configuracion, "IP_BROKER");
+
+	char *puertoTeam = config_get_string_value(archivo_de_configuracion, "PUERTO_TEAM");
+	char *ipTeam = config_get_string_value(archivo_de_configuracion, "IP_TEAM");
+
+	char *puertoGameCard = config_get_string_value(archivo_de_configuracion, "PUERTO_GAMECARD");
+	char *ipGamecard = config_get_string_value(archivo_de_configuracion, "IP_GAMECARD");
+
+	t_log *logger = iniciar_log("GameBoy");
+
+	log_info(logger,"PUERTO BROKER: %s IP BROKER: %s\n", puertoBroker, ipBroker);
+
+	log_info(logger,"PUERTO TEAM: %s IP TEAM: %s\n", puertoTeam, ipTeam);
+
+	log_info(logger,"PUERTO GAMECARD: %s IP GAMECARD: %s\n", puertoGameCard, ipGamecard);
+
+
+
+	//int conexion = conectarse_a_un_servidor(ip,puerto,logger);
+
+	/*
 	Serialize_PackAndSend_NEW_POKEMON(conexion, 1,"PIKACHU", 5, 5, 10);
 	log_info(logger,"Se envio mensaje: %i, %s, x: %i, y: %i, cant: %i\n", 1,"PIKACHU", 5, 5, 10);
 
@@ -21,6 +39,7 @@ int main(void) {
 
 	Serialize_PackAndSend_CAUGHT_POKEMON(conexion, 4, 0);
 	log_info(logger,"Se envio mensaje: %i, %s, %i\n", 3,"BULBASUAR", 0);
+	*/
 
 	return EXIT_SUCCESS;
 }
