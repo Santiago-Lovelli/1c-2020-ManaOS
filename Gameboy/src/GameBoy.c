cdecb4dceaa554a664420485dfda032452937e7e
Juan Anello <juan.manuel.anello@gmail.com>
2020-04-23 22:10:55 -0300

    Mas avances en Gameboy!

--- Gameboy/src/GameBoy.c
+++ Gameboy/src/GameBoy.c
@@ -1,25 +1,28 @@
 #include "GameBoy.h"
 
-t_log *logger;
-
 void New_pokemon(char *argv[]){
-
+	int conexion = conectarA(argv[2]);
+	//Serialize
 }
 
 void Appeared_pokemon(char *argv[]){
-
+	int conexion = conectarA(argv[2]);
+	//Serialize
 }
 
 void Catch_pokemon(char *argv[]){
-
+	int conexion = conectarA(argv[2]);
+	//Serialize
 }
 
 void Caught_pokemon(char *argv[]){
-
+	int conexion = conectarA(argv[2]);
+	//Serialize
 }
 
 void Get_pokemon(char *argv[]){
-
+	int conexion = conectarA(argv[2]);
+	//Serialize
 }
 
 int obtenerNroMensaje(char *actual){
@@ -45,24 +48,99 @@ int obtenerNroProceso(char *actual){
 		return d_GAMECARD;
 	else if( string_equals_ignore_case(actual,"SUSCRIPTOR") )
 		return d_SUBSCRIBER;
+	return 99;
 }
 
-void cumplirPedido(char *argv[]){
+char* obtenerPuertoProceso(char *actual){
+	switch ( obtenerNroProceso(actual) ){
+	case d_BROKER:;
+		return puertoBroker;
+		break;
+	case d_TEAM:;
+		return puertoTeam;
+		break;
+	case d_GAMECARD:;
+		return puertoGameCard;
+		break;
+	default:;
+		return "-1";
+		break;
+	}
+}
+
+char* obtenerIpProceso(char *actual){
+	switch ( obtenerNroProceso(actual) ){
+		case d_BROKER:;
+			return ipBroker;
+			break;
+		case d_TEAM:;
+			return ipTeam;
+			break;
+		case d_GAMECARD:;
+			return ipGamecard;
+			break;
+		default:;
+			return "-1";
+			break;
+		}
+}
+
+int conectarA(char *actual){
+	return conectarse_a_un_servidor( obtenerIpProceso(actual) , obtenerPuertoProceso(actual) , logger);
+}
+
+void cumplirPedido(int argc, char *argv[]){
 	switch( obtenerNroMensaje(argv[3]) ) {
+
 	case d_NEW_POKEMON:;
+		if(argc < 8){
+		    log_info(logger,"Argumentos insuficientes para la operacion: %s", argv[3]);
+		    break;
+		}
 		log_info(logger,"RECIBI EL PEDIDO: %s PARA EL PROCESO: %s", argv[3], argv[2]);
+		New_pokemon(argv);
 		break;
+
 	case d_APPEARED_POKEMON:;
+	    if(argc < 7){
+	    	log_info(logger,"Argumentos insuficientes para la operacion: %s", argv[3]);
+	    	break;
+	    }
 		log_info(logger,"RECIBI EL PEDIDO: %s PARA EL PROCESO: %s", argv[3], argv[2]);
+		Appeared_pokemon(argv);
+		/*log_info(logger,"POKEMON: %s ", argv[4]);
+		log_info(logger,"POSX: %i ", atoi( argv[5] ) );
+		log_info(logger,"POSY: %i ", atoi( argv[6] ) );*/
 		break;
+
 	case d_CATCH_POKEMON:;
+		if(argc < 7){
+			log_info(logger,"Argumentos insuficientes para la operacion: %s", argv[3]);
+			break;
+		}
 		log_info(logger,"RECIBI EL PEDIDO: %s PARA EL PROCESO: %s", argv[3], argv[2]);
+		Catch_pokemon(argv);
 		break;
+
 	case d_CAUGHT_POKEMON:;
+		if(argc < 6){
+			log_info(logger,"Argumentos insuficientes para la operacion: %s", argv[3]);
+			break;
+		}
 		log_info(logger,"RECIBI EL PEDIDO: %s PARA EL PROCESO: %s", argv[3], argv[2]);
+		Caught_pokemon(argv);
 		break;
+
 	case d_GET_POKEMON:;
+		if(argc < 5){
+			log_info(logger,"Argumentos insuficientes para la operacion: %s", argv[3]);
+			break;
+		}
 		log_info(logger,"RECIBI EL PEDIDO: %s PARA EL PROCESO: %s", argv[3], argv[2]);
+		Get_pokemon(argv);
+		break;
+	case d_SUBSCRIBE_QUEUE:;
+		//pendiente
 		break;
 	default:;
 		log_info(logger,"DEFAULT");
@@ -73,20 +151,14 @@ void cumplirPedido(char *argv[]){
 int main(int argc, char *argv[]) {
 	t_config* archivo_de_configuracion = config_create("../Gameboy.config");
 
-	char *proceso = "";
-	proceso = argv[2];
+	puertoBroker = config_get_string_value(archivo_de_configuracion, "PUERTO_BROKER");
+	ipBroker = config_get_string_value(archivo_de_configuracion, "IP_BROKER");
 
-	char *mensaje = "";
-	mensaje = argv[3];
+	puertoTeam = config_get_string_value(archivo_de_configuracion, "PUERTO_TEAM");
+	ipTeam = config_get_string_value(archivo_de_configuracion, "IP_TEAM");
 
-	char *puertoBroker = config_get_string_value(archivo_de_configuracion, "PUERTO_BROKER");
-	char *ipBroker = config_get_string_value(archivo_de_configuracion, "IP_BROKER");
-
-	char *puertoTeam = config_get_string_value(archivo_de_configuracion, "PUERTO_TEAM");
-	char *ipTeam = config_get_string_value(archivo_de_configuracion, "IP_TEAM");
-
-	char *puertoGameCard = config_get_string_value(archivo_de_configuracion, "PUERTO_GAMECARD");
-	char *ipGamecard = config_get_string_value(archivo_de_configuracion, "IP_GAMECARD");
+	puertoGameCard = config_get_string_value(archivo_de_configuracion, "PUERTO_GAMECARD");
+	ipGamecard = config_get_string_value(archivo_de_configuracion, "IP_GAMECARD");
 
 	logger = iniciar_log("GameBoy");
 
@@ -94,7 +166,7 @@ int main(int argc, char *argv[]) {
 	log_info(logger,"PUERTO TEAM: %s IP TEAM: %s\n", puertoTeam, ipTeam);
 	log_info(logger,"PUERTO GAMECARD: %s IP GAMECARD: %s\n", puertoGameCard, ipGamecard);
 
-	cumplirPedido(argv);
+	cumplirPedido(argc,argv);
 
 	/*
 	int conexionBroker = conectarse_a_un_servidor(ipBroker,puertoBroker,logger);
--- Gameboy/src/GameBoy.h
+++ Gameboy/src/GameBoy.h
@@ -8,4 +8,33 @@
 #include <Logger/Logger.h>
 #include <Serializacion/Serializacion.h>
 
+////////////////////////
+// VARIABLES GLOBALES //
+////////////////////////
+
+t_log *logger;
+char *puertoBroker;
+char *ipBroker;
+char *puertoTeam;
+char *ipTeam;
+char *puertoGameCard;
+char *ipGamecard;
+
+////////////////
+// FUNCIONES  //
+///////////////
+
+void New_pokemon(char *argv[]);
+void Appeared_pokemon(char *argv[]);
+void Catch_pokemon(char *argv[]);
+void Caught_pokemon(char *argv[]);
+void Get_pokemon(char *argv[]);
+int obtenerNroMensaje(char *actual);
+int obtenerNroProceso(char *actual);
+char* obtenerPuertoProceso(char *actual);
+char* obtenerIpProceso(char *actual);
+int conectarA(char *actual);
+void cumplirPedido(int argc, char *argv[]);
+
+
 #endif /* GAMEBOY_H_ */
