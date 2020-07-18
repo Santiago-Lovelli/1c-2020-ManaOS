04815b699587887b0d792b69c14aa9223438d36f
lovelli.santiago.9@gmail.com <lovelli.santiago.9@gmail.com>
2020-07-18 15:12:51 -0300

    Considero broken 2

---
+++ scripts/scriptCompilarBroker2.sh
@@ -0,0 +1,8 @@
+echo "Comienzo compilacion Broker"
+cd /home/utnso/workspace/tp-2020-1c-ManaOS-/Broker2/
+mkdir Debug
+cd Debug
+mkdir src
+gcc -I"/home/utnso/workspace/tp-2020-1c-ManaOS-/ComunParaTodos" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Broker2.d" -MT"src/Broker2.o" -o "src/Broker2.o" "../src/Broker2.c"
+gcc -L"/home/utnso/workspace/tp-2020-1c-ManaOS-/ComunParaTodos/Debug" -o "Broker"  ./src/Broker.o   -lComunParaTodos -lcommons -lpthread
+echo "Termine compilacion Broker"
