9a8100e3269127514102bf8b17441f7b08262084
lovelli.santiago.9@gmail.com <lovelli.santiago.9@gmail.com>
2020-07-18 13:20:55 -0300

    Compilando el Team

---
+++ scripts/scriptCompilarTeam.sh
@@ -0,0 +1,10 @@
+
+echo "Comienzo compilacion Team"
+cd /home/utnso/workspace/tp-2020-1c-ManaOS-/Team/
+mkdir Debug
+cd Debug
+mkdir src
+gcc -I"/home/utnso/workspace/tp-2020-1c-ManaOS-/ComunParaTodos" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Team.d" -MT"src/Team.o" -o "src/Team.o" "../src/Team.c"
+gcc -L"/home/utnso/workspace/tp-2020-1c-ManaOS-/ComunParaTodos/Debug" -o "Team"  ./src/Team.o   -lcommons -lpthread -lComunParaTodos
+
+echo "Compile Team"
