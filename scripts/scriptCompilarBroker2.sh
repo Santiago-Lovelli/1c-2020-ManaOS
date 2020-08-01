31c8aae575be9e8c120868764bbfc4d2b2df6830
lovelli.santiago.9@gmail.com <lovelli.santiago.9@gmail.com>
2020-08-01 16:22:27 -0300

    Hago bien el script

--- scripts/scriptCompilarBroker2.sh
+++ scripts/scriptCompilarBroker2.sh
@@ -4,5 +4,5 @@ mkdir Debug
 cd Debug
 mkdir src
 gcc -I"/home/utnso/workspace/tp-2020-1c-ManaOS-/ComunParaTodos" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Broker2.d" -MT"src/Broker2.o" -o "src/Broker2.o" "../src/Broker2.c"
-gcc -L"/home/utnso/workspace/tp-2020-1c-ManaOS-/ComunParaTodos/Debug" -o "Broker"  ./src/Broker.o   -lComunParaTodos -lcommons -lpthread
+gcc -L"/home/utnso/workspace/tp-2020-1c-ManaOS-/ComunParaTodos/Debug" -o "Broker2"  ./src/Broker2.o   -lComunParaTodos -lcommons -lpthread
 echo "Termine compilacion Broker"
