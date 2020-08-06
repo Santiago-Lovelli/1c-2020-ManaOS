c905a5772c7f50b5d3ae00d5564dac37eedb3c2a
AnelloJM <juan.manuel.anello@gmail.com>
2020-08-06 20:06:28 -0300

    Chau echo

--- scripts/script_para_shared.sh
+++ scripts/script_para_shared.sh
@@ -2,5 +2,5 @@
 
 cd /home/utnso/
 
-echo \ "export LD_LIBRARY_PATH="\$"LD_LIBRARY_PATH:/home/utnso/workspace/tp-2020-1c-ManaOS-/ComunParaTodos/Debug" >> .bashrc
+export LD_LIBRARY_PATH="\$"LD_LIBRARY_PATH:/home/utnso/workspace/tp-2020-1c-ManaOS-/ComunParaTodos/Debug
 
